f8146e1533153e7c640523cd89fa9a744d811f5b
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2026-06-08 13:32:46 +0300

    first commit

---
+++ src/postsData.js
@@ -0,0 +1,167 @@
+export const postsData = [
+  {
+    id: 1,
+    img: "/image1.webp",
+    uploadDate: "11-04-2016",
+    likesToday: 128,
+    commentsToday: 31,
+    history: {
+      "2016-08-09": { likes: 67, comments: 22 },
+      "2016-12-09": { likes: 95, comments: 40 },
+    },
+  },
+  {
+    id: 2,
+    img: "/image2.webp",
+    uploadDate: "12-04-2016",
+    likesToday: 215,
+    commentsToday: 45,
+    history: {
+      "2016-08-09": { likes: 110, comments: 18 },
+      "2016-12-09": { likes: 150, comments: 35 },
+    },
+  },
+  {
+    id: 3,
+    img: "/image3.webp",
+    uploadDate: "14-04-2016",
+    likesToday: 88,
+    commentsToday: 12,
+    history: {
+      "2016-08-09": { likes: 45, comments: 5 },
+      "2016-12-09": { likes: 70, comments: 10 },
+    },
+  },
+  {
+    id: 4,
+    img: "/image4.webp",
+    uploadDate: "15-04-2016",
+    likesToday: 340,
+    commentsToday: 89,
+    history: {
+      "2016-08-09": { likes: 190, comments: 50 },
+      "2016-12-09": { likes: 280, comments: 75 },
+    },
+  },
+  {
+    id: 5,
+    img: "/image5.webp",
+    uploadDate: "11-04-2016",
+    likesToday: 128,
+    commentsToday: 31,
+    history: {
+      "2016-08-09": { likes: 67, comments: 22 },
+      "2016-12-09": { likes: 95, comments: 40 },
+    },
+  },
+  {
+    id: 6,
+    img: "/image6.webp",
+    uploadDate: "12-04-2016",
+    likesToday: 215,
+    commentsToday: 45,
+    history: {
+      "2016-08-09": { likes: 110, comments: 18 },
+      "2016-12-09": { likes: 150, comments: 35 },
+    },
+  },
+  {
+    id: 7,
+    img: "/image7.webp",
+    uploadDate: "14-04-2016",
+    likesToday: 88,
+    commentsToday: 12,
+    history: {
+      "2016-08-09": { likes: 45, comments: 5 },
+      "2016-12-09": { likes: 70, comments: 10 },
+    },
+  },
+  {
+    id: 8,
+    img: "/image8.webp",
+    uploadDate: "15-04-2016",
+    likesToday: 340,
+    commentsToday: 89,
+    history: {
+      "2016-08-09": { likes: 190, comments: 50 },
+      "2016-12-09": { likes: 280, comments: 75 },
+    },
+  },
+  {
+    id: 9,
+    img: "/image9.webp",
+    uploadDate: "12-04-2016",
+    likesToday: 215,
+    commentsToday: 45,
+    history: {
+      "2016-08-09": { likes: 110, comments: 18 },
+      "2016-12-09": { likes: 150, comments: 35 },
+    },
+  },
+  {
+    id: 10,
+    img: "/image10.webp",
+    uploadDate: "14-04-2016",
+    likesToday: 88,
+    commentsToday: 12,
+    history: {
+      "2016-08-09": { likes: 45, comments: 5 },
+      "2016-12-09": { likes: 70, comments: 10 },
+    },
+  },
+  {
+    id: 11,
+    img: "/image11.webp",
+    uploadDate: "15-04-2016",
+    likesToday: 340,
+    commentsToday: 89,
+    history: {
+      "2016-08-09": { likes: 190, comments: 50 },
+      "2016-12-09": { likes: 280, comments: 75 },
+    },
+  },
+  {
+    id: 12,
+    img: "/image12.webp",
+    uploadDate: "11-04-2016",
+    likesToday: 128,
+    commentsToday: 31,
+    history: {
+      "2016-08-09": { likes: 67, comments: 22 },
+      "2016-12-09": { likes: 95, comments: 40 },
+    },
+  },
+  {
+    id: 13,
+    img: "/image13.webp",
+    uploadDate: "12-04-2016",
+    likesToday: 215,
+    commentsToday: 45,
+    history: {
+      "2016-08-09": { likes: 110, comments: 18 },
+      "2016-12-09": { likes: 150, comments: 35 },
+    },
+  },
+  {
+    id: 14,
+    img: "/image14.webp",
+    uploadDate: "14-04-2016",
+    likesToday: 88,
+    commentsToday: 12,
+    history: {
+      "2016-08-09": { likes: 45, comments: 5 },
+      "2016-12-09": { likes: 70, comments: 10 },
+    },
+  },
+  {
+    id: 15,
+    img: "/image15.webp",
+    uploadDate: "15-04-2016",
+    likesToday: 340,
+    commentsToday: 89,
+    history: {
+      "2016-08-09": { likes: 190, comments: 50 },
+      "2016-12-09": { likes: 280, comments: 75 },
+    },
+  },
+];
